8f48be262403c982e995e930c008aa17bf349df6
Bryan D. Tumba <bryan.delama@utec.edu.pe>
2019-04-02 21:27:12 -0500

    Working

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main() {
-    int a, b;
-    cin >> a;
-    cin >> b;
-
-    cout << a + b;
+    int n;
+    cin >> n;
+    map<string, int> n_country;
+    for (int i = 0; i < n; i++){
+        string country;
+        cin >> country;
+        string Gio_loved;
+        getline(cin, Gio_loved);
+    }
     system("pause");
     return 0;
 }
